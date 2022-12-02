6e8ef58d8b37286872d102c481fccb47f26695c2
Nurbakhyt <nurbakhyt.bolatov@narxoz.kz>
2022-12-02 22:47:26 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import './App.css';
+import Header from './components/Header'
+import CharacterTable from './components/CharacterTable'
+import axios from 'axios'
+import React , {useEffect,useState} from 'react'
+import Search from './components/Search'
+
+const hash =  "bd0722d5750b6362d5ba0212ca36726b"
+
+function App() {
+  const[items,setItems] = useState([])
+  const[isLoading,setLoading] = useState(true)
+  const [query,setQuery] = useState('')
+
+  useEffect(()=>{
+      const fetch = async()=>{
+        if(query===''){
+
+            const result = await axios(`http://gateway.marvel.com/v1/public/characters?ts=1&apikey=344d40df0c8cc373141c1dc321fae9cf&hash=${hash}`)
+            console.log(result.data.data.results)
+            setItems(result.data.data.results)
+            setLoading(false) 
+
+          
+          
+        }else{
+          const result = await axios(`http://gateway.marvel.com/v1/public/characters?nameStartsWith=${query}&ts=1&apikey=344d40df0c8cc373141c1dc321fae9cf&hash=${hash}`)
+          console.log(result.data.data.results)
+          setItems(result.data.data.results)
+          setLoading(false)
+        }
+      
+    }
+
+    fetch()
+  },[query])
+
+  return (
+    <div className="container">
+      <Header />
+      <Search search={(q)=>setQuery(q)}></Search>
+      <CharacterTable items={items} isLoading={isLoading} />
+    </div>
+  );
+}
+
+export default App;
